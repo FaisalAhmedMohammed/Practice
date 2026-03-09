ffdc9fea07774b1622e85ac6218a4a5946cb334b
Faisal Ahmed <mohammedfaisalahmed873@gmail.com>
2026-03-09 07:40:13 +0530

    added new vairable and printing

--- src/main/java/com/ahmed/co/demo/Application.java
+++ src/main/java/com/ahmed/co/demo/Application.java
@@ -9,10 +9,13 @@ public class Application {
 	public static void main(String[] args) {
 
 		int a = 10;
-		
+
 		SpringApplication.run(Application.class, args);
 	   
 	     int b = 20;
+
+		 int c = 30;
+		 System.out.println(b);
 	}
 
 }
